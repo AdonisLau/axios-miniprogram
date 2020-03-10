250e1329f2fc22d612614f9f16460207057d9d10
Adonis.Lau <lauweeking@gmail.com>
2020-03-10 11:15:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,55 @@
+# 基于axios的小程序适配器 #
+
+## 用法 ##
+
+* 下载个[axios](https://github.com/axios/axios/edit/master/dist/axios.min.js)
+* 下载个`axios`的[小程序适配器](https://github.com/AdonisLau/axios-miniprogram-adapter/adapter.js)
+* 安装一下
+
+```javascript
+import axios from './axios';
+import adapter from './adapter';
+
+let instance = axios.create({
+  adapter,
+  // 必须
+  transformRequest: []
+});
+```
+
+## 使用 ##
+
+> 常规的http请求请查看[axios文档](https://github.com/axios/axios)。新增以下两个方法供上传和下载使用。
+
+```javascript
+// 上传
+const promise = instance({
+  method: 'upload',             // 上传专属方法
+  url: 'xxx',                   // 开发者服务器地址
+  name: 'file',                 // 文件对应的 key，开发者在服务端可以通过这个 key 获取文件的二进制内容
+  filePath: 'xxx',              // 要上传文件资源的路径 (本地路径)
+  data: {},                     // 额外参数 
+  onUploadProgress(e) {         // 获取上传进度   
+    console.log(e);
+  }
+});
+
+// 下载
+const promise = instance({
+  method: 'download',           // 下载专属方法
+  url: 'xxx',                   // 下载资源的 url
+  filePath: 'xxx',              // 指定文件下载后存储的路径 (本地路径)
+  onDownloadProgress(e) {       // 获取下载进度
+    console.log(e);
+  }
+});
+
+/**
+ * res返回 { tempFilePath: 'xxx', filePath: 'xxx' }
+ **/
+promise.then(res => {
+
+});
+
+
+```
---
+++ adapter.js
@@ -0,0 +1,233 @@
+/**
+ * 基于axios对wx.request wx.upload 进行封装，后续可用于async await
+ */
+function noop() { }
+
+function formatParams(params) {
+  let result = [];
+
+  for (let i in params) {
+    result.push(encodeURIComponent(i) + '=' + encodeURIComponent(params[i]));
+  }
+
+  return result.join('&');
+}
+
+function isAbsolute(url) {
+  return /^(?:http|https|\/\/)/.test(url);
+}
+
+function settle(resolve, reject, response) {
+  let validateStatus = response.config.validateStatus;
+  if (!validateStatus || validateStatus(response.status)) {
+    resolve(response);
+  } else {
+    reject(createError(
+      'Request failed with status code ' + response.status,
+      response.config,
+      null,
+      response.request,
+      response
+    ));
+  }
+}
+
+function createError(message, config, code, request, response) {
+  let error = new Error(message);
+  return enhanceError(error, config, code, request, response);
+}
+
+function enhanceError(error, config, code, request, response) {
+  error.config = config;
+  if (code) {
+    error.code = code;
+  }
+  error.request = request;
+  error.response = response;
+  return error;
+}
+
+const METHOD = {
+  defaults: 'request',
+  upload: 'uploadFile',
+  download: 'downloadFile'
+};
+
+const BEFORE = {
+  defaults(options, config) {
+    options.data = config.data;
+    options.method = (config.method || 'get').toUpperCase();
+  },
+
+  upload(options, config) {
+    options.name = config.name;
+    options.filePath = config.filePath;
+    options.formData = options.data || {};
+  },
+
+  download(options, config) {
+    if (options.filePath) {
+      options.filePath = config.filePath;
+    }
+  }
+};
+
+const AFTER = {
+  defaults: noop,
+
+  upload(request, config) {
+    if (config.onUploadProgress) {
+      request.onProgressUpdate(config.onUploadProgress);
+    }
+  },
+
+  download(request, config) {
+    if (config.onDownloadProgress) {
+      request.onProgressUpdate(config.onDownloadProgress);
+    }
+  }
+};
+
+const COMPLETE = {
+  defaults: noop,
+
+  upload(request, config) {
+    if (config.onUploadProgress) {
+      request.offProgressUpdate(config.onUploadProgress);
+    }
+  },
+
+  download(request, config) {
+    if (config.onDownloadProgress) {
+      request.offProgressUpdate(config.onDownloadProgress);
+    }
+  }
+};
+
+const SUCCESS = {
+  defaults(res, response) {
+    response.data = res.data;
+  },
+
+  download(res, response) {
+    let data = response.data = {};
+
+    if (res.filePath) {
+      data.filePath = res.filePath;
+    }
+
+    if (res.tempFilePath) {
+      data.tempFilePath = res.tempFilePath;
+    }
+  }
+}
+
+function getMap(map, method) {
+  return map[method] || map.defaults;
+}
+
+/**
+ * 基于axios的wx.request和wx.uploadFile适配器
+ */
+export default function adapter(config) {
+  return new Promise((resolve, reject) => {
+    let url = config.url;
+
+    if (config.params) {
+      let params = formatParams(config.params);
+
+      if (params) {
+        url += (url.indexOf('?') >= 0 ? '&' : '?') + params;
+      }
+    }
+
+    if (!isAbsolute(url)) {
+      url = config.baseURL + url;
+    }
+
+    let request = null;
+    let status = 'pending';
+    let method = config.method || 'get';
+    let responseType = config.responseType;
+
+    let options = {
+      url,
+      header: config.headers,
+      dataType: responseType || 'json',
+      responseType: responseType === 'arraybuffer' ? 'arraybuffer' : 'text',
+
+      success(res) {
+        if (status !== 'pending') {
+          return;
+        }
+
+        status = 'succeed';
+
+        if (request) {
+          getMap(COMPLETE, method)(request, config);
+        }
+
+        res.header.cookies = res.cookies || [];
+
+        let response = {
+          status: res.statusCode,
+          statusText: res.errMsg,
+          headers: res.header,
+          config: config,
+          request: request
+        };
+
+        getMap(SUCCESS, method)(res, response);
+
+        settle(resolve, reject, response);
+        
+        request = null;
+      },
+
+      fail(e) {
+        if (status !== 'pending') {
+          return;
+        }
+
+        status = 'failed';
+
+        if (request) {
+          getMap(COMPLETE, method)(request, config);
+        }
+  
+        reject(createError(e.errMsg || 'Network Error', config, null, request));
+
+        request = null;
+      }
+    };
+
+    if (config.timeout) {
+      options.timeout = config.timeout;
+    }
+
+    getMap(BEFORE, method)(options, config);
+
+    let m = getMap(METHOD, method);
+
+    request = wx[m](options);
+
+    getMap(AFTER, method)(request, config);
+
+    if (config.cancelToken) {
+
+      config.cancelToken.promise.then(cancel => {
+        if (!request) {
+          return;
+        }
+
+        getMap(COMPLETE, method)(request, config);
+
+        request.abort();
+
+        request = null;
+
+        reject(cancel);
+      });
+    }
+  });
+}
